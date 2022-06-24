6d1b1c8c74940e58af679f6bc9dca0b701e8492e
Marhlon Korb <marhlonkorb@hotmail.com>
2022-06-24 00:32:49 -0300

    First commit
    Criação da classe Produto, divisão dos pacotes(model, repository, service) e criação da classe abstrata ProdutoRepository.
    Criação da classe ProdutoService sem campos por enquanto

---
+++ src/main/java/com/api/produtos/model/Produto.java
@@ -0,0 +1,57 @@
+package com.api.produtos.model;
+
+
+import java.text.DateFormat;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+public class Produto {
+
+    long id;
+    String nome;
+    double preco;
+
+    public Produto(Long id, String nome, double preco) {
+        super();
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return id == produto.id && Double.compare(produto.preco, preco) == 0 && Objects.equals(nome, produto.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, preco);
+    }
+}
---
+++ src/main/java/com/api/produtos/service/ProdutoService.java
@@ -0,0 +1,4 @@
+package com.api.produtos.service;
+
+public class ProdutoService {
+}
